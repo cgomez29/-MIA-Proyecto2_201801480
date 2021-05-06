1c951abe4f647e848db0ea30355f141f8709d845
Cristian Gomez <2912196670108@ingenieria.usac.edu.gt>
2021-05-06 01:59:36 -0600

    empezando a llenar la tabla

--- quiniela-server/controller/adminController.go
+++ quiniela-server/controller/adminController.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"../database"
 	"../models"
 	"fmt"
 	"github.com/gofiber/fiber"
@@ -8,21 +9,63 @@ import (
 
 func BulkLoad(c *fiber.Ctx) error  {
 	var data models.Temporal
+	var user models.USUARIO
+
 	if err:= c.BodyParser(&data); err != nil {
 		return err
 	}
-	//query := fmt.Sprintf("CALL sp_insert_deporte('%s','%s','%s')", data.Nombre, data.Imagen, data.Color)
-	//_, err := database.ExecuteQuery(query)
+	var query string
+	//INSERTANDO USUARIO
+	query = fmt.Sprintf("CALL sp_insert_usuario_bl('%s','%s','%s','%s')",
+		data.Username, data.Password, data.Nombre, data.Apellido)
+	_,err := database.ExecuteQuery(query)
 
-	fmt.Println("NOMBRE ",data.Nombre)
+	if err != nil  {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "Usuario repetido",
+		})
+	}
 
-	/*if err != nil {
+	// Recuperando id de usuario ingresado
+	query = fmt.Sprintf("SELECT idUsuario FROM USUARIO WHERE username = %s", data.Username)
+
+	rows,err := database.ExecuteQuery(query)
+
+	if err != nil  {
 		c.Status(fiber.StatusInternalServerError)
 		return c.JSON(fiber.Map{
 			"message": "Error running query",
 			"error": err,
 		})
-	}*/
+	}
 
-	return c.JSON(data)
+	for rows.Next() {
+		rows.Scan(&user.IdUsuario)
+	}
+
+	for _, i:= range data.Resultados {
+		//INSERTANDO TEMPORADAS
+		//formando la fecha
+		dateTemporada := fmt.Sprintf("%s%s%s%s",i.Temporada[0:4],"/",i.Temporada[6:],"/01")
+		query = fmt.Sprintf("CALL sp_insert_temporada_bl('%s','%s')", i.Temporada, dateTemporada)
+		database.ExecuteQuery(query)
+		for _, j:= range i.Jornadas {
+
+			for _, k:= range j.Predicciones {
+				// INSERTANDO DEPORTES
+				query = fmt.Sprintf("CALL sp_insert_deporte_bl('%s')", k.Deporte)
+				database.ExecuteQuery(query)
+			}
+
+		}
+	}
+
+	/*
+
+	//*/
+
+	return c.JSON(fiber.Map{
+		"message": "Successful",
+	})
 }
